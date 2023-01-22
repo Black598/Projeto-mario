3e24e09a78bc0ae707cd208f5fda3a9a4dfab70b
Douglas Carvalho <doug.oak598@gmail.com>
2023-01-22 17:07:45 -0300

    add do index js e mostrar video do trailer

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const exitModal = document.querySelector(".fechar-modal")
+
+const modal = document.querySelector(".modal")
+const Video = document.getElementById ("video")
+const linkDoVideo = Video.src;
+
+function alternarModal() {
+    modal.classList.toggle("aberto")
+
+}
+
+
+botaoTrailer.addEventListener("click", ()=> {
+
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+
+
+})
+
+exitModal.addEventListener("click", () =>{
+
+    alternarModal();
+    video.setAttribute("src", "")
+    
+})
+
+
+
